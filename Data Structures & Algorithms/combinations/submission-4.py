9e2e93f3b6f881df84a74ce0dce404fdef2fd83b
SI THU LIN AUNG <142065360+C2linaung@users.noreply.github.com>
2026-09-24 14:40:01 +0800

    Add: combinations - submission-4

---
+++ Data Structures & Algorithms/combinations/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        output = []
+        def backtrack(i, path):
+            if len(path) == k:
+                output.append(path[:])
+                return
+            
+            for j in range(i, n + 1):
+                path.append(j)
+                backtrack(j + 1, path)
+                path.pop()
+        
+        backtrack(1, [])
+        return output
